a6384c66a61363b585e96393397957158a7e815e
Nguyen Anh Tuan(TuanNA) <tuanna@mti-tech.vn>
2026-03-30 22:35:45 +0700

    chore: update JS resolvers [rc] 20260330

---
+++ rc/rophim10.co/v1/all_cinema.js
@@ -0,0 +1,1 @@
+async function getAllCinema(e=1){try{return console.log("[KENG][RoPhim] getAllCinema() - Note: Site does not have dedicated Cinema category."),JSON.stringify([])}catch(e){return JSON.stringify([])}}
